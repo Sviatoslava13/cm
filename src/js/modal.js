1782e2013ba90a52e43be16b21ced040e09efc92
Sviatoslava13 <sviatoslavamar@gmail.com>
2023-03-01 10:14:30 +0200

    +

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector('.hero__btn');
+const tourBtn = document.querySelector('.tour__button');
 const modal = document.querySelector('.modal');
 const btnClose = document.querySelector('.modal__button');
 const body = document.querySelector('#body');
 btn.addEventListener('click', handleOpenModal);
+tourBtn.addEventListener('click', handleOpenModal);
 btnClose.addEventListener('click', handleOpenModal);
 //const hero = document.querySelector('.hero__container');
    //  body.classList.add('active')
